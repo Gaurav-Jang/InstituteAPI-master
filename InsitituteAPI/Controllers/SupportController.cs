898c1569dac16b4c26dbf3aa09029ca370cd1666
Pushkar Sharma <ps100016@google.com>
2024-06-21 13:53:25 +0530

    Support page completed with post method, some minor fixes in AddClassRoom and AddStudent.

---
+++ InsitituteAPI/Controllers/SupportController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using InstituteAPI.BusinessServiceLayer.Interfaces;
+using InstituteAPI.Models.Support;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InstituteAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SupportController : ControllerBase
+    {
+
+        private readonly ILogger<SupportController> _logger;
+        private readonly ISupportService _supportService;
+        public SupportController(ILogger<SupportController> logger, ISupportService SupportService)
+        {
+            _logger = logger;
+            _supportService = SupportService;
+        }
+        
+        ///Post
+        [HttpPost]
+        [Route("SetSupport")]
+        public IActionResult SetSupport([FromBody] Support support)
+        {
+
+            int SupportId = _supportService.SetSupport(support);
+            return Ok(SupportId);
+        }
+    }
+}
